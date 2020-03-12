7c9153e815b1290366552a30a0cb27b459285f76
Leandro <ldstudio.com@gmail.com>
2020-03-12 10:57:29 -0300

    Compatibility update

--- src/Facades/ServerRequest.php
+++ src/Facades/ServerRequest.php
@@ -2,18 +2,25 @@
 
 namespace SMartins\PassportMultiauth\Facades;
 
-use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Component\HttpFoundation\Request;
 
 class ServerRequest
 {
     /**
-     * @todo Switch deprecated DiactorosFactory by PsrHttpFactory
      * @param Request $symfonyRequest
-     * @return \Psr\Http\Message\RequestInterface|\Psr\Http\Message\ServerRequestInterface|\Zend\Diactoros\ServerRequest
+     * @return \Psr\Http\Message\RequestInterface|\Psr\Http\Message\ServerRequestInterface
      */
     public static function createRequest(Request $symfonyRequest)
     {
-        return (new DiactorosFactory())->createRequest($symfonyRequest);
+        if (class_exists(PsrHttpFactory::class)) {
+            $psr17Factory = new Psr17Factory;
+            
+            return (new PsrHttpFactory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory))
+                ->createRequest($symfonyRequest);
+        }
+        
+        throw new Exception('Unable to resolve PSR request. Please install symfony/psr-http-message-bridge and nyholm/psr7.');
     }
 }
